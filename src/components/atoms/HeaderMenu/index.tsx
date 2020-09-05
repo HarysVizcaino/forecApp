d126cf22c3d86c500d0434f36fd056393ef7388b
Harys vizcaino <harys@mctekk.com>
2020-09-05 19:13:49 -0400

    added action to home menu

--- src/components/atoms/HeaderMenu/index.tsx
+++ src/components/atoms/HeaderMenu/index.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import { View, Text} from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 import { Colors } from '../../../styles';
+import { TOUCHABLE_AREA } from '../../../contants';
 
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
 import { faSun, faTemperatureLow, faWater, faEllipsisV  } from '@fortawesome/free-solid-svg-icons'
 
 const HeaderMenu = () => (
-  <View>
+  <TouchableOpacity hitSlop={TOUCHABLE_AREA}>
    <FontAwesomeIcon size={25} icon={ faEllipsisV } color={Colors.WHITE} />
-  </View>
+  </TouchableOpacity>
 )
 
 export default HeaderMenu;
--- src/contants.ts
+++ src/contants.ts
@@ -1,1 +1,9 @@
+const TOUCH_PADDING = 18;
+
 export const MOON_IMAGE = './assets/images/moon.png';
+export const TOUCHABLE_AREA = {
+  top: TOUCH_PADDING,
+  bottom: TOUCH_PADDING,
+  left: TOUCH_PADDING,
+  right: TOUCH_PADDING,
+};
